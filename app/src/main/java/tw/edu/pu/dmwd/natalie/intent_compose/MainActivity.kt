d7886332a2e57c4e296f6060c0f274f95927694b
natalie <o1103003@gm.pu.edu.tw>
2023-05-10 15:03:56 +0800

    navigation

---
+++ app/src/main/java/tw/edu/pu/dmwd/natalie/intent_compose/MainActivity.kt
@@ -0,0 +1,258 @@
+package tw.edu.pu.dmwd.natalie.intent_compose
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.rounded.AccountBox
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import tw.edu.pu.dmwd.natalie.intent_compose.ui.theme.Intent_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Intent_composeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FirstScreen(navController: NavController) {
+    Column(modifier = Modifier
+
+        .fillMaxSize()
+
+        .background(Color.Yellow),
+
+        verticalArrangement = Arrangement.Center,
+
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+
+        Button(onClick = {
+
+            navController.navigate("JumpSecond")
+
+        }) {
+
+            Text(text = "我是畫面1，按一下跳至畫面2")
+
+        }
+        Text(text = "台中高美濕地")
+        Image(painter = painterResource(id = R.drawable.gaomei) , contentDescription ="image" )
+
+    }
+
+}
+
+@Composable
+
+fun SecondScreen(navController: NavController) {
+
+    Column(modifier = Modifier.fillMaxSize(),
+
+        verticalArrangement = Arrangement.Center,
+
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+
+        Button(onClick = {
+
+            navController.navigate("JumpThird")
+
+        }) {
+
+            Text(text = "我是畫面2，按一下跳至畫面3")
+
+        }
+        Text(text = "台中彩虹村")
+        Image(painter = painterResource(id = R.drawable.rainbow) , contentDescription ="image" )
+    }
+
+}
+@Composable
+
+fun ThirdScreen(navController: NavController) {
+
+
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Red),
+
+        verticalArrangement = Arrangement.Center,
+
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+
+        Button(onClick = {
+
+            navController.navigate("JumpFirst")
+
+        }) {
+
+            Text(text = "我是畫面3，按一下跳至畫面1")
+
+        }
+        Text(text = "台中武陵農場")
+        Image(painter = painterResource(id = R.drawable.wuling) , contentDescription ="image" )
+    }
+
+}
+
+@Composable
+fun Greeting(name: String) {
+    val navController = rememberNavController()
+    val context = LocalContext.current
+    val showMenu = remember {
+
+        mutableStateOf(false)
+
+    }
+    Column {
+
+        TopAppBar(
+
+            title = { Text(text = "頁面轉換實例") },
+            navigationIcon = {
+
+                IconButton(onClick = {
+
+                    Toast.makeText(context, "您點選了導覽圖示", Toast.LENGTH_SHORT).show()
+
+                }) {
+
+                    Icon(Icons.Default.Menu, contentDescription = "Navigation icon")
+
+                }
+
+            },
+            actions = {
+
+                IconButton(
+
+                    onClick = { Toast.makeText(context, "作者：锺爱丽", Toast.LENGTH_SHORT).show() }
+
+                ) {
+
+                    Icon(Icons.Rounded.AccountBox, contentDescription = "author")
+
+                }
+                IconButton(
+
+                    onClick = { showMenu.value = true }
+
+                ) {
+
+                    Icon(Icons.Default.MoreVert, contentDescription = null)
+
+                }
+                DropdownMenu(
+
+                    expanded = showMenu.value, onDismissRequest = { showMenu.value = false }
+
+                ) {
+
+                    DropdownMenuItem(
+
+                        onClick = {
+
+                            navController.navigate("JumpFirst")
+
+                        })
+
+                    {
+
+                        Text(text = "畫面1")
+
+                    }
+
+                    DropdownMenuItem(
+
+                        onClick =
+
+                        {
+
+                            navController.navigate("JumpSecond")
+
+                        })
+
+                    {
+
+                        Text(text = "畫面2")
+
+                    }
+                    DropdownMenuItem(
+
+                        onClick =
+
+                        {
+
+                            navController.navigate("JumpThird")
+
+                        })
+
+                    {
+
+                        Text(text = "畫面3")
+
+                    }
+
+                }
+
+            }
+
+        )
+        NavHost(navController = navController, startDestination = "JumpFirst") {
+
+            composable("JumpFirst") {
+                FirstScreen(navController = navController)
+
+            }
+
+            composable("JumpSecond") {
+
+                SecondScreen(navController = navController)
+
+            }
+            composable("JumpThird") {
+
+                ThirdScreen(navController = navController)
+
+            }
+
+        }
+    }
+}
+
